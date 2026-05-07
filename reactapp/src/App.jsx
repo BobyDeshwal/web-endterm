2237a34c452ed5671c777c156ebe2efcd0f9b1de
Boby Deshwal <boby43773@gmail.com>
2026-05-07 11:23:40 +0530

    first commit

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,18 +1,43 @@
-import  { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+
+
+function Child(props) {
+  return (
+    <div>
+      <h2>Name: {props.name}</h2>
+      <h2>Age: {props.age}</h2>
+      <hr />
+    </div>
+  );
+}
+
 
 function App() {
+  // Props data
+  const name = "jack";
+  const age = 19;
+
+
   const [users, setUsers] = useState([]);
+
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((res) => res.json())
-      .then((data) => setUsers(data));
+      .then((data) => setUsers(data))
+      .catch((error) => console.log(error));
   }, []);
 
   return (
     <div>
+      <h1>Parent Component</h1>
+
+      {}
+      <Child name={name} age={age} />
+
       <h1>User List</h1>
 
+      {}
       {users.map((user) => (
         <div key={user.id}>
           <p>Username: {user.username}</p>
@@ -24,7 +49,4 @@ function App() {
   );
 }
 
-
 export default App;
-
-
